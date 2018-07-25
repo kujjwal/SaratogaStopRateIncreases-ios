3f081a16d0ce88180824896929a12b69999334a5
kujjwal <ujju.cooldude@gmail.com>
2018-07-24 19:00:59 -0700

    Initial Commit

---
+++ Silicon Valley WaterTests/Silicon_Valley_WaterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Silicon_Valley_WaterTests.swift
+//  Silicon Valley WaterTests
+//
+//  Created by Ujjwal Krishnamurthi on 7/24/18.
+//  Copyright © 2018 Ujjwal Krishnamurthi. All rights reserved.
+//
+
+import XCTest
+@testable import Silicon_Valley_Water
+
+class Silicon_Valley_WaterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
